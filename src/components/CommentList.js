e76dc27192411b87f367eac5723534ef808397a8
anto004 <anto_004@live.com>
2020-05-15 12:16:41 +0530

    Display comments in CommentList

--- src/components/CommentList.js
+++ src/components/CommentList.js
@@ -1,6 +1,31 @@
 /* eslint-disable react/no-unescaped-entities */
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
-export default () => {
-	return <div>I'm a Comment List</div>;
-};
+// TODO: Generate unique ID
+
+class CommentList extends Component {
+	renderComments() {
+		const { comments } = this.props;
+
+		return comments.map(({ id, comment }) => {
+			return <li key={id}>{comment}</li>;
+		});
+	}
+
+	render() {
+		return (
+			<div>
+				<ul>{this.renderComments()}</ul>
+			</div>
+		);
+	}
+}
+
+function mapStateToProps(state) {
+	return {
+		comments: state.comments,
+	};
+}
+
+export default connect(mapStateToProps)(CommentList);
